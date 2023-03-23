016c9a1d6959816f27cf13f350dd78c2e34a4b86
Pedro Pimienta M <pedro.pimienta.morales@gmail.com>
2023-03-22 20:07:04 -0500

    new class model created

---
+++ src/models/busquedas.ts
@@ -0,0 +1,11 @@
+export class Busquedas {
+    historial = ['Tegucigalpa', 'Madrid', 'San Jose', 'San Francisco'];
+
+    constructor() {}
+
+    async getCity(lugar: string) {
+        console.log(lugar);
+
+        return [];
+    }
+}
